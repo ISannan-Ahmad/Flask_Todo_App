800ef5dd1041f7bbcb6653c3916ed85d4003e990
Sannan Ahmad <sannanahmad.16@gmail.com>
2025-05-07 02:52:23 +0500

    Removed React Frontend

--- frontend/src/components/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({ children }) => {
-  const isLoggedIn = localStorage.getItem("user");
-  return isLoggedIn ? children : <Navigate to="/login" />;
-};
-
-export default ProtectedRoute;
